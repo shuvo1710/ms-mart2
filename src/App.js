03887828dda2ed8a622228cf490e6dd37aaa8c2d
shuvo1710 <mdshovo102002@gmail.com>
2023-02-03 10:15:14 +0600

    2nd time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>shuvo</h1>
-     
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+      <h1>hi</h1>
     </div>
   );
 }

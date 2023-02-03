660904b29026aa00d6f54580be12413f2c7dd7d0
shuvo1710 <mdshovo102002@gmail.com>
2023-02-03 17:37:07 +0600

    added home page

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Navbar from '../Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Component/Home/Home";
 import Navbar from "../Component/Navbar/Navbar";
 import Main from "../Layout/Main";
 
@@ -9,7 +10,7 @@ export const routes=createBrowserRouter([
         children:[
             {
                 path:"/",
-                element:<Navbar></Navbar>
+                element:<Home></Home>
             },
             
         ]

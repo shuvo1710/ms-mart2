f85be05eb0d925b68b89f1e7ba0020f35df7bffa
shuvo1710 <mdshovo102002@gmail.com>
2023-02-03 12:15:59 +0600

    added navbar

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,3 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import Navbar from "../Component/Navbar/Navbar";
+import Main from "../Layout/Main";
 
-export const routes=createBrowserRouter([])
+export const routes=createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:"/",
+                element:<Navbar></Navbar>
+            }
+        ]
+    }
+])

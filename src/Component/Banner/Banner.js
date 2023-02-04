cc8ed8460844c024ae8745d434f8b899796146a8
shuvo1710 <mdshovo102002@gmail.com>
2023-02-04 13:16:12 +0600

    banner shuvo

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -1,32 +1,69 @@
 import React, { useRef, useState } from "react";
 import './Banner.css'
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 // import required modules
-import { Navigation } from "swiper";
-// import required modules
+import { Pagination,Autoplay } from "swiper";
+
 
 
 const Banner = () => {
     return (
-        <div>
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <div className="my-10">
+            <Swiper
+             autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[ Pagination,Autoplay]}
+               className="mySwiper bannerSwiper">
                 <SwiperSlide>
-                    <div className="banner-container">
-                        <div className="banner-content">
+                    <div className="relative bannerImg2">
+                        <div className="absolute top-1/4">
+                            <div className="px-10">
 
+                                <h3 className="text-3xl font-semibold">Dresses And Jumpsuits</h3>
+                                <h1 className="text-6xl font-bold">Daisy Dress</h1>
+                                <div className="my-10">
+                                    <button className="px-4 py-2 text-black border border-black bg-white">Shop Now</button>
+                                </div>
+                            </div>
+                            
                         </div>
                     </div>
                 </SwiperSlide>
 
+                <SwiperSlide>
+                    <div className="relative bannerImg">
+                        <div className="absolute top-1/4">
+                            <div className="px-10">
+
+                                <h3 className="text-3xl font-semibold">Dresses And Jumpsuits</h3>
+                                <h1 className="text-6xl font-bold">Daisy Dress</h1>
+                                <div className="my-10">
+                                    <button className="px-4 py-2 text-black border border-black bg-white">Shop Now</button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
             </Swiper>
         </div>
     );
 };
 
 export default Banner;
+
+
 
 
 

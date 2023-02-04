e5b9a6c58f4db629c36d0b3753d3cc8502fba562
Sajib Ahmed <sajib7315@gmail.com>
2023-02-04 13:16:10 +0600

    addin sajb code

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Card from '../Navbar/Card/Card';
+
 import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
-            <Navbar></Navbar>
+            
             <Banner></Banner>
+            <Card></Card>
         </div>
     );
 };
